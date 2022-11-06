a888891e2ac37104c0e9bdbda106af56e95594bb
zhuyasen <g.zhufuyi@gmail.com>
2022-11-07 00:02:37 +0800

    optimize main code

---
+++ cmd/serverNameExample_httpExample/initial/registerClose.go
@@ -0,0 +1,44 @@
+package initial
+
+import (
+	"context"
+	"time"
+
+	"github.com/zhufuyi/sponge/internal/config"
+	"github.com/zhufuyi/sponge/internal/model"
+
+	"github.com/zhufuyi/sponge/pkg/app"
+	"github.com/zhufuyi/sponge/pkg/tracer"
+)
+
+// RegisterClose 注册app需要释放的资源
+func RegisterClose(servers []app.IServer) []app.Close {
+	var closes []app.Close
+
+	// 关闭服务
+	for _, s := range servers {
+		closes = append(closes, s.Stop)
+	}
+
+	// 关闭mysql
+	closes = append(closes, func() error {
+		return model.CloseMysql()
+	})
+
+	// 关闭redis
+	if config.Get().App.CacheType == "redis" {
+		closes = append(closes, func() error {
+			return model.CloseRedis()
+		})
+	}
+
+	// 关闭trace
+	if config.Get().App.EnableTracing {
+		closes = append(closes, func() error {
+			ctx, _ := context.WithTimeout(context.Background(), 2*time.Second) //nolint
+			return tracer.Close(ctx)
+		})
+	}
+
+	return closes
+}
---
+++ cmd/serverNameExample_mixExample/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/zhufuyi/sponge/cmd/serverNameExample_mixExample/initial"
+
+	"github.com/zhufuyi/sponge/pkg/app"
+)
+
+// @title serverNameExample api docs
+// @description http server api docs
+// @schemes http https
+// @version v0.0.0
+// @host localhost:8080
+func main() {
+	initial.Config()
+	servers := initial.RegisterServers()
+	closes := initial.RegisterClose(servers)
+
+	a := app.New(servers, closes)
+	a.Run()
+}
